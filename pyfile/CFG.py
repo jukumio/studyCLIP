f805c59d7943a649045742cc4e39aeee7faa7ec1
jukumio <ghju516@naver.com>
2024-06-04 17:36:04 +0900

    second_commit

---
+++ pyfile/CFG.py
@@ -0,0 +1,32 @@
+class CFG:
+    debug = False
+    image_path = image_path
+    captions_path = captions_path
+    batch_size = 32
+    num_workers = 2
+    head_lr = 1e-3
+    image_encoder_lr = 1e-4
+    text_encoder_lr = 1e-5
+    weight_decay = 1e-3  
+    patience = 1
+    factor = 0.8
+    epochs = 4
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model_name = 'resnet50'
+    image_embedding = 2048
+    text_encoder_model = "distilbert-base-uncased"
+    text_embedding = 768
+    text_tokenizer = "distilbert-base-uncased"
+    max_length = 200
+
+    pretrained = True # 이미지 인코더, 텍스트 인코더 둘 다
+    trainable = True # 이미지 인코더, 텍스트 인코더 둘 다
+    temperature = 1.0
+
+    # 이미지사이즈
+    size = 224
+
+    num_projection_layers = 1
+    projection_dim = 256
+    dropout = 0.1
---
+++ pyfile/build_loaders.py
@@ -0,0 +1,15 @@
+def build_loaders(dataframe, tokenizer, mode):
+    transforms = get_transforms(mode=mode)
+    dataset = CLIPDataset(
+        dataframe["image"].values,
+        dataframe["caption"].values,
+        tokenizer=tokenizer,
+        transforms=transforms,
+    )
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=CFG.batch_size,
+        num_workers=CFG.num_workers,
+        shuffle=True if mode == "train" else False,
+    )
+    return dataloader
