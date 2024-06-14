74ac5a322db93f38dcf891588541a43f4e335fd4
jukumio <ghju516@naver.com>
2024-06-14 14:26:35 +0900

    edit_6_14

--- pyfile/simpleCLIP.py
+++ pyfile/simpleCLIP.py
@@ -7,13 +7,13 @@ import itertools
 from tqdm.autonotebook import tqdm
 import albumentations as A
 import matplotlib.pyplot as plt
+import zipfile
 
 import torch
 from torch import nn
 import torch.nn.functional as F
 import timm
 from transformers import DistilBertModel, DistilBertConfig, DistilBertTokenizer
-import AvgMeter, build_loaders, CFG, CLIP, cross_entropy, Encoder, Epoch, get_TFs, main, make_train_valid_dfs, ProjectionHead
 import subprocess
 
 os.environ['KAGGLE_USERNAME'] = "juuhoney"
@@ -27,17 +27,28 @@ print("Unzipping dataset...")
 with zipfile.ZipFile("flickr8k.zip", 'r') as zip_ref:
     zip_ref.extractall("flickr8k")
 
-# dataset 변수 설정
-dataset = "8k"
+# 압축 해제된 데이터 경로 설정
+dataset_dir = "flickr8k"
+image_dir = os.path.join(dataset_dir, "Images")
+captions_file_8k = os.path.join(dataset_dir, "captions.txt")
+
+dataset = "8k"  # "8k" 또는 "30k"로 설정
 
 if dataset == "8k":
-  df = pd.read_csv("captions.txt")
-  df['id'] = [id_ for id_ in range(df.shape[0] // 5) for _ in range(5)]
-  df.to_csv("captions.csv", index=False)
-  df = pd.read_csv("captions.csv")
-  image_path = "/content/Images"
-  captions_path = "/content"
-elif dataset == "30k":
+    # 캡션 파일 경로
+    captions_file = captions_file_8k
+
+    # 데이터프레임 로드 및 처리
+    df = pd.read_csv(captions_file)
+    df['id'] = [id_ for id_ in range(df.shape[0] // 5) for _ in range(5)]
+    df.to_csv(os.path.join(dataset_dir, "captions.csv"), index=False)
+
+    # 데이터프레임 다시 로드
+    df = pd.read_csv(os.path.join(dataset_dir, "captions.csv"))
+    image_path = image_dir
+    captions_path = dataset_dir
+
+'''elif dataset == "30k":
   df = pd.read_csv("/content/flickr30k_images/results.csv", delimiter="|")
   df.columns = ['image', 'caption_number', 'caption']
   df['caption'] = df['caption'].str.lstrip()
@@ -48,10 +59,11 @@ elif dataset == "30k":
   df['id'] = ids
   df.to_csv("captions.csv", index=False)
   image_path = "/content/flickr30k_images/flickr30k_images"
-  captions_path = "/content"
+  captions_path = "/content"'''
 
 df.head()
 
+import AvgMeter, build_loaders, CFG, CLIP, cross_entropy, Encoder, Epoch, get_TFs, main, make_train_valid_dfs, ProjectionHead
 
 # A simple Example
 
